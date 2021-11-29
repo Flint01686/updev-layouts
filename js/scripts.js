268eb0d9f664e9e786db22b2959c255cad04f4f2
Flint01686 <kirill01686@gmail.com>
2021-11-29 18:00:16 +0400

    updev9

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,4 @@
 
- 
- var tag = document.createElement('script');
-
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-  // 3. This function creates an <iframe> (and YouTube player)
-  //    after the API code downloads.
-  var player;
-  function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-      height: '390',
-      width: '640',
-      videoId: 'Ryp5FjHX4Wc',
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
-  }
-
-  // 4. The API will call this function when the video player is ready.
-  function onPlayerReady(event) {
-    event.target.playVideo();
-  }
-
-  // 5. The API calls this function when the player state changes.
-  //    The function indicates that when playing a video (state=1),
-  //    the player should play for six seconds and then stop.
-  var done = false;
-  function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-      //setTimeout(stopVideo, 6000);
-      done = true;
-    }
-  }
-  function stopVideo() {
-    player.stopVideo();
-  }
-
 
  $(document).ready(function () {
 
@@ -55,7 +14,13 @@
      return false;
    });
 
-
+   $('.mobile-policy-btn,.policy__side a').click(function () {
+    if ($('.policy__side,.mobile-policy-btn,body').hasClass('open-policy')) {
+      $('.policy__side,.mobile-policy-btn,body').removeClass('open-policy');
+    } else {
+      $('.policy__side,.mobile-policy-btn,body').addClass('open-policy');
+    }
+  });
 
    $('.burger').click(function () {
      if ($('.main-menu,.burger,body').hasClass('open')) {
